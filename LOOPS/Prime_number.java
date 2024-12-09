d0bdb67d5af80917cefc121bff7fc40de744d3e2
DEVANSH KUMAR <kumardevansh12345@gmail.com>
2024-12-10 00:32:48 +0530

    Add files via upload

---
+++ LOOPS/Prime_number.java
@@ -0,0 +1,14 @@
+package LOOPS;
+
+import java.util.Scanner;
+
+public class Prime_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n ; i++){
+            
+            
+        }
+    }
+}
