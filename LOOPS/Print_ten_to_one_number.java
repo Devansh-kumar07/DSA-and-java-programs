1d9fc70b5dc38f5e48a54e9b9c6146a5322a3910
DEVANSH KUMAR <kumardevansh12345@gmail.com>
2024-12-11 14:34:36 +0530

    Add files via upload

---
+++ LOOPS/Print_ten_to_one_number.java
@@ -0,0 +1,9 @@
+package LOOPS;
+public class Print_ten_to_one_number {
+    public static void main(String[] args) {
+        for( int i = 10 ; i >= 1 ; i-- ){
+            System.out.println(i);
+        }
+    }
+    
+}
